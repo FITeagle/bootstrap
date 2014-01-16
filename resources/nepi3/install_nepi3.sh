158e9223bb5f79647c157df8550a7f810ff4624f
AlexanderWillner <alexander.willner@tu-berlin.de>
2014-01-16 08:59:39 +0100

    setting up nepi3

---
+++ resources/nepi3/install_nepi3.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+function removeOldRuby {
+ apt-get -y --purge remove ruby-rvm ruby ruby1.9.3
+ rm -rf /usr/share/ruby-rvm /etc/rvmrc /etc/profile.d/rvm.sh
+ apt-get -y autoremove
+}
+
+function installNeededLibs {
+ apt-get install -y build-essential libxml2-dev libxslt-dev libssl-dev
+}
+
+function installRuby {
+ \curl -L https://get.rvm.io | bash -s stable
+ source /etc/profile.d/rvm.sh
+ rvm install ruby-1.9.3-p286 --autolibs=4
+ rvm use ruby-1.9.3-p286
+ rvm gemset create omf
+ rvm use ruby-1.9.3-p286@omf --default
+
+ rvm current; ruby -v
+}
+
+function installNepiEC {
+ (
+ curl -s http://nepi.inria.fr/code/nepi/archive/652bc2e46cfe.tar.gz|tar xzf -
+ mv nepi-652bc2e46cfe nepi
+ echo "Setup the environment:"
+ echo "  export PYTHONPATH=\$PYTHONPATH:$(pwd)/src"
+
+ curl -s -O https://raw.github.com/fiteagle/bootstrap/master/resources/nepi3/nepi3_example_ping.py
+ echo "Run the first experiment:"
+ echo "  python nepi3_example_ping.py"
+ )
+}
+
+function startOmfRc {
+ start omf_rc
+}
+
+#removeOldRuby
+#installNeededLibs
+#installRuby
+installNepiEC
+#startOmfRc
