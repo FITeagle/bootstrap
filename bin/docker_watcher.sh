d8f55e2a7cd074e8b3887d0995a9c7b024f3139d
Björn Riemer <bjoern.riemer@tu-berlin.de>
2015-04-13 18:39:14 +0200

    added watch script for docker rereation via cron job

---
+++ bin/docker_watcher.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+# cron job for rebuilding the fiteagle Docker
+# date +%F_%T --date=@$(cat /tmp/fiteagle_state_file.txt)
+
+_CONFIG_TRIGGER_FILE="/tmp/fiteagle_state_file.txt"
+#_CONFIG_FT_BOOTSTAP_PATH=""
+_docker_path=""
+ECHO="echo run "
+ECHO=""
+
+if [ -f ${_CONFIG_TRIGGER_FILE} ] ; then
+	echo "downloading Dockerfile..."
+	_docker_path=$(mktemp -d)
+	wget -q https://github.com/FITeagle/bootstrap/raw/master/docker/Dockerfile -O "${_docker_path}/Dockerfile" || (echo "download failed!"; rm -rf "${_docker_path}"; exit 1)
+	echo "rebuild docker 'fiteagle2bin'..."
+	CMD="docker build $1 --rm --no-cache --tag=fiteagle2bin ${_docker_path}"
+	$ECHO $CMD || (echo "docker failed!"; rm -rf "${_docker_path}"; exit 1)
+	rm -rf "${_docker_path}"
+	echo "shutdown old docker container 'ft2'..."
+	CMD="docker stop ft2"
+	$ECHO $CMD
+	echo "remove old docker container 'ft2'..."
+	CMD="docker rm ft2"
+	$ECHO $CMD
+	echo "starting new container 'ft2'..."
+	CMD="docker run -d --name=ft2 -p 8443:8443 --env WILDFLY_ARGS="" fiteagle2bin"
+	$ECHO $CMD
+	echo "moving state_file..."
+	CMD="mv ${_CONFIG_TRIGGER_FILE} ${_CONFIG_TRIGGER_FILE}.bak"
+	$ECHO $CMD
+else
+	echo "nothing to do"
+fi
