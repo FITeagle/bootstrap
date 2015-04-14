5e80dbc59a8995bfefd72f1a8fadf78a7e542519
Björn Riemer <bjoern.riemer@tu-berlin.de>
2015-04-14 15:24:35 +0200

    added -f command to docker watch

--- bin/docker_watcher.sh
+++ bin/docker_watcher.sh
@@ -7,12 +7,12 @@ _CONFIG_TRIGGER_FILE="/tmp/fiteagle_state_file.txt"
 #ECHO="echo run "
 ECHO=""
 
-if [ -f ${_CONFIG_TRIGGER_FILE} ] ; then
+if [ -f ${_CONFIG_TRIGGER_FILE} ] || [ "x$1" = "x-f" ] ; then
 	echo "downloading Dockerfile..."
 	_docker_path=$(mktemp -d)
 	wget -q https://github.com/FITeagle/bootstrap/raw/master/docker/Dockerfile -O "${_docker_path}/Dockerfile" || (echo "download failed!"; rm -rf "${_docker_path}"; exit 1)
 	echo "rebuild docker 'fiteagle2bin'..."
-	CMD="docker build $1 --rm --no-cache --tag=fiteagle2bin ${_docker_path}"
+	CMD="docker build --rm --no-cache --tag=fiteagle2bin ${_docker_path}"
 	$ECHO $CMD || (echo "docker failed!"; rm -rf "${_docker_path}"; exit 1)
 	rm -rf "${_docker_path}"
 	echo "shutdown old docker container 'ft2'..."
@@ -24,9 +24,12 @@ if [ -f ${_CONFIG_TRIGGER_FILE} ] ; then
 	echo "starting new container 'ft2'..."
 	CMD='docker run -d --name=ft2 -p 8443:8443 --env WILDFLY_ARGS="" fiteagle2bin'
 	$ECHO $CMD
-	echo "moving state_file..."
-	CMD="mv ${_CONFIG_TRIGGER_FILE} ${_CONFIG_TRIGGER_FILE}.bak"
-	$ECHO $CMD
+	if [ "x$1" = "x-f" ] ; then 
+		echo "moving state_file..."
+		CMD="mv ${_CONFIG_TRIGGER_FILE} ${_CONFIG_TRIGGER_FILE}.bak"
+		$ECHO $CMD
+	fi
 else
 	#echo "nothing to do"
+	true
 fi
