87e4d88f149716a9e54cefff07aa21872c4c0297
Bjoern Riemer <bjoern.riemer@tu-berlin.de>
2015-07-29 15:29:22 +0200

    added Dockerfile based on very small alpine linux made docker image name configurable

--- bin/docker_watcher.sh
+++ bin/docker_watcher.sh
@@ -13,6 +13,7 @@ _CONFIG_LOCAL_BUILD_PATH=$(dirname `readlink -f $0`)/../docker/Dockerfile
 _CONFIG_DOT_FITEAGLE="/root/.fiteagle"
 #_CONFIG_FAST_REBUILD_FROM_CACHE=1
 _CONFIG_EXPOSE_PORT_ARGS="-p 8443:8443" #-p 8443:8443 -p 8080:8080 -p 9990:9990" #expose management interface on port 9990
+_CONFIG_DOCKER_IMAGE_NAME="fiteagle2bin"
 
 [ -f "./docker_watcher.config" ] && . ./docker_watcher.config
 
@@ -45,7 +46,7 @@ cleanup() {
 }
 
 run_docker_ft2() {
-	runcmd "docker run -d --name=ft2 ${DOCKER_RUN_VOLUMES} ${DOCKER_RUN_ARGS} fiteagle2bin" || die "docker failed!"
+	runcmd "docker run -d --name=ft2 ${DOCKER_RUN_VOLUMES} ${DOCKER_RUN_ARGS} ${_CONFIG_DOCKER_IMAGE_NAME}" || die "docker failed!"
 }
 
 start_docker_ft2() {
@@ -63,12 +64,13 @@ fi
 
 if [ -f ${_CONFIG_TRIGGER_FILE} ] || [ "x$1" = "x-f" ] ; then
 	echo "Rebuild of fiteagle docker requested....."
-	runcmd "docker tag fiteagle2bin:latest fiteagle2bin:current"
-	runcmd "docker rmi fiteagle2bin:latest"
+	runcmd "docker tag ${_CONFIG_DOCKER_IMAGE_NAME}:latest ${_CONFIG_DOCKER_IMAGE_NAME}:current"
+	runcmd "docker rmi ${_CONFIG_DOCKER_IMAGE_NAME}:latest"
 	echo "downloading Dockerfile..."
 	_docker_path=$(mktemp -d)
 	if [ "${_CONFIG_LOCAL_BUILD}" = "1" ] ; then
-		cp ${_CONFIG_LOCAL_BUILD_PATH} ${_docker_path}/Dockerfile_ || die "fiteagle-bootstrap/docker/Dockerfile not found!!"
+		cp -v ${_CONFIG_LOCAL_BUILD_PATH} ${_docker_path}/Dockerfile_ || die "fiteagle-bootstrap/docker/Dockerfile not found!!"
+		cp -v $(dirname ${_CONFIG_LOCAL_BUILD_PATH})/* ${_docker_path}
 	else
 		wget -q https://github.com/FITeagle/bootstrap/raw/master/docker/Dockerfile -O "${_docker_path}/Dockerfile_" || (echo "download failed!"; cleanup "${_docker_path}"; exit 1)
 	fi
@@ -77,8 +79,8 @@ if [ -f ${_CONFIG_TRIGGER_FILE} ] || [ "x$1" = "x-f" ] ; then
 	else
 		sed "s/DUMMY/$(date +%s)/g" ${_docker_path}/Dockerfile_ >${_docker_path}/Dockerfile
 	fi 
-	echo "rebuild docker 'fiteagle2bin'..."
-	if runcmd "docker build --rm ${DOCKER_BUILD_ARGS} --tag=fiteagle2bin ${_docker_path}" ; then
+	echo "rebuild docker '${_CONFIG_DOCKER_IMAGE_NAME}'..."
+	if runcmd "docker build --rm ${DOCKER_BUILD_ARGS} --tag=${_CONFIG_DOCKER_IMAGE_NAME} ${_docker_path}" ; then
 		echo ok;
 	else
 		cleanup "${_docker_path}"
@@ -87,9 +89,9 @@ if [ -f ${_CONFIG_TRIGGER_FILE} ] || [ "x$1" = "x-f" ] ; then
 	rm -rf "${_docker_path}"
 	echo "shutdown old docker container 'ft2'..."
 	runcmd "docker stop ft2"
-	echo "remove old docker container 'ft2' and image 'fiteagle2bin'..."
+	echo "remove old docker container 'ft2' and image '${_CONFIG_DOCKER_IMAGE_NAME}'..."
 	runcmd "docker rm ft2"
-	runcmd "docker rmi fiteagle2bin:current"
+	runcmd "docker rmi ${_CONFIG_DOCKER_IMAGE_NAME}:current"
 	echo "starting new container 'ft2'..."
 	run_docker_ft2
 	if [ "x$1" != "x-f" ] ; then 
