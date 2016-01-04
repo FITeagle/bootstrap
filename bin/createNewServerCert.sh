a1a41816be63deaa8af9484ef6048fcd1adaa0ce
Bjoern Riemer <bjoern.riemer@tu-berlin.de>
2016-01-04 12:50:40 +0100

    added script to create user certificates

--- bin/createNewServerCert.sh
+++ bin/createNewServerCert.sh
@@ -14,6 +14,8 @@ openssl req -batch -new -out ${URL}.csr -key ${URL}.key -config ${CERT}
 openssl req -text -noout -in ${URL}.csr
 openssl x509 -req -days ${DAYS} -in ${URL}.csr -signkey ${URL}.key -out ${URL}.crt -extensions v3_req -extfile ${CERT}
 openssl pkcs12 -export -name root -in ${URL}.crt -inkey ${URL}.key -out ${URL}.p12
+
+#adding cert&key to jetty keystore
 keytool -importkeystore -destkeystore ${KEYSTORE}.keystore -srckeystore ${URL}.p12 -srcstoretype pkcs12 -alias root \
   -srcstorepass ${PWD} -deststorepass ${PWD} -destkeypass ${PWD} -srckeypass ${PWD}
  
